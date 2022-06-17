c568fa75237869bddd303791d98658f2fc742280
abeel50 <abeelmuhammad50@gmail.com>
2022-06-17 20:54:46 +0500

    100%

--- Brackets.py
+++ Brackets.py
@@ -7,10 +7,10 @@ def solution(S):
     for i in S:
         if i =='{' or i == '(' or i == '[':
             stack.append(i)
-        elif i== '}' and len(stack) != 0 and stack.pop() != '{':
-            return 0
-        elif i== ')' and len(stack) != 0 and stack.pop() != '(':
-            return 0
-        elif i== ']' and len(stack) != 0 and stack.pop() != '[':
+        elif (i== '}' or i == ')' or i ==']' ) and len(stack) != 0:
+            t = stack.pop() + i
+            if t!= "()" and t != "{}" and t!= "[]":
+                return 0
+        else:
             return 0
     return 1 if len(stack) == 0 else 0
